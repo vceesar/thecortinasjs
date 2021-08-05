b4e42c625111c6b793e99d61c19f095eb7c16099
Victor Cesar <victorc1104@gmail.com>
2021-08-05 04:27:22 -0300

    - db info

--- models/MySQLConnection.js
+++ models/MySQLConnection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var dados_conexaobanco = {
     connectionLimit: 10,
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b8e66a9c407b22',
-    password: '1840c6fb',
-    database: 'heroku_923d2c6928001a1',
+    host: 'yourdatabaseinfo',
+    user: 'yourdatabaseinfo',
+    password: 'yourdatabaseinfo',
+    database: 'yourdatabaseinfo',
     multipleStatements: true
 };
 
